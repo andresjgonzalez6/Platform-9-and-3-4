9fa2df96c2c9a3701af2fdd94e2194e807d01f31
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-11-24 23:06:57 -0700

    ajax bad still

--- routes/edeamAPI.js
+++ routes/edeamAPI.js
@@ -6,10 +6,9 @@ let nutrition_APP_ID = process.env.Nutrition_APP_ID;
 let nutrition_APP_KEY = process.env.Nutrition_APP_KEY;
 let foodDB_APP_ID = process.env.FoodDB_APP_ID;
 let foodDB_APP_KEY = process.env.FoodDB_APP_KEY;
-let query = "biscuts";
 let goodStuff = {
     recipeSearch: function (query) {
-        let recipeURL = "https://api.edamam.com/search?q=" + query+ "&app_id=${" + recipe_APP_ID + "}&app_key=${" + recipe_APP_KEY +"}"
+        let recipeURL = "https://api.edamam.com/search?q=" + "biscuts" + "&app_id=${" + recipe_APP_ID + "}&app_key=${" + recipe_APP_KEY +"}"
         $.ajax({
             url: recipeURL,
             method: "GET"
