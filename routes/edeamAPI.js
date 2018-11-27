c8ca8180c1a8a65e3ad5625b81c3f09609f72584
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-11-27 00:04:12 -0700

    sill having issues with ajax calls

--- routes/edeamAPI.js
+++ routes/edeamAPI.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+let $ = require("jquery");
 
 let recipe_APP_ID = process.env.Recipe_APP_ID;
 let recipe_APP_KEY = process.env.Recipe_APP_KEY;
@@ -6,21 +7,16 @@ let nutrition_APP_ID = process.env.Nutrition_APP_ID;
 let nutrition_APP_KEY = process.env.Nutrition_APP_KEY;
 let foodDB_APP_ID = process.env.FoodDB_APP_ID;
 let foodDB_APP_KEY = process.env.FoodDB_APP_KEY;
+let query = "biscuts";
 let goodStuff = {
     recipeSearch: function (query) {
-        let recipeURL = "https://api.edamam.com/search?q=" + "biscuts" + "&app_id=${" + recipe_APP_ID + "}&app_key=${" + recipe_APP_KEY +"}"
-        $.ajax({
-            url: recipeURL,
-            method: "GET"
-        }).then(function (res) {
-            let recipeRes = res.data
-            console.log(recipeRes)
-        })
+        let recipeURL = "https://api.edamam.com/search?q=" + query + "&app_id=${" + recipe_APP_ID + "}&app_key=${" + recipe_APP_KEY +"}"
+       $.get({})
     },
     nutritionSearch: function () {},
 
     foodDBAPI: function () {}
 }
-goodStuff.recipeSearch();
+goodStuff.recipeSearch(query);
 
 module.exports = goodStuff;
