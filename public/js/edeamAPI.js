ba7cddf4d068082e74cf526099ebb4c1974a9ad7
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-11-27 21:00:48 -0700

    AJAX IS ALIVE!!!!!!!!

--- public/js/edeamAPI.js
+++ public/js/edeamAPI.js
@@ -1,21 +1,19 @@
-require('dotenv').config();
-
-let recipe_APP_ID = process.env.Recipe_APP_ID;
-let recipe_APP_KEY = process.env.Recipe_APP_KEY;
-let nutrition_APP_ID = process.env.Nutrition_APP_ID;
-let nutrition_APP_KEY = process.env.Nutrition_APP_KEY;
-let foodDB_APP_ID = process.env.FoodDB_APP_ID;
-let foodDB_APP_KEY = process.env.FoodDB_APP_KEY;
+let app_id = "055b5671";
+let app_key = "1502e188414b9f8a78b54045f49fe008";
 let query = "biscuts";
 let goodStuff = {
     recipeSearch: function (query) {
-        let recipeURL = "https://api.edamam.com/search?q=" + query + "&app_id=${" + recipe_APP_ID + "}&app_key=${" + recipe_APP_KEY +"}"
-       $.get({})
+        let recipeURL = "https://api.edamam.com/search?q=" + query + "&app_id=" + app_id + "&app_key=" + app_key;
+        $.ajax({
+            url: recipeURL,
+            method: "GET"
+        })
+            .then(function (res) {
+                console.log(res);
+            });
     },
-    nutritionSearch: function () {},
+    nutritionSearch: function () { },
 
-    foodDBAPI: function () {}
+    foodDBAPI: function () { }
 }
 goodStuff.recipeSearch(query);
-
-module.exports = goodStuff;
