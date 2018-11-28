01ffee7e3067f2061b801489b2cc6ef5e2815190
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-11-27 21:39:19 -0700

    trying to make it work sadness so far......

--- public/js/edeamAPI.js
+++ public/js/edeamAPI.js
@@ -1,19 +1,36 @@
-let app_id = "055b5671";
-let app_key = "1502e188414b9f8a78b54045f49fe008";
-let query = "biscuts";
+let Recipe_APP_ID = "055b5671";
+let Recipe_APP_KEY = "1502e188414b9f8a78b54045f49fe008";
+let Nutrition_APP_ID = "cd84f084";
+let Nutrition_APP_KEY = "5913f23d52bd629ff917b766ac7eee62";
+let FoodDB_APP_ID = "2d32703d";
+let FoodDB_APP_KEY = "4638b7482a7a3cdcbcad3726aabf4fdb";
+let image;
 let goodStuff = {
     recipeSearch: function (query) {
-        let recipeURL = "https://api.edamam.com/search?q=" + query + "&app_id=" + app_id + "&app_key=" + app_key;
+        let recipeURL = "https://api.edamam.com/search?q=" + query + "&app_id=" + Recipe_APP_ID + "&app_key=" + Recipe_APP_KEY;
         $.ajax({
             url: recipeURL,
             method: "GET"
         })
             .then(function (res) {
+                for (let i = 0; i < res.hits.length; i++) {
+                console.log(res.hits[i].recipe.image)
+                }
                 console.log(res);
+                
             });
     },
-    nutritionSearch: function () { },
+    nutritionSearch: function (query) { },
 
-    foodDBAPI: function () { }
+    foodDBAPI: function (query) {
+        let 
+    }
 }
-goodStuff.recipeSearch(query);
+
+$('#search-button').on('click', function(event) {
+    event.preventDefault();
+    let query = $("#serach-input").val();
+    goodStuff.recipeSearch(query);
+    console.log(query);
+    
+});
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,4 @@
 var db = require("../models");
-// require('dotenv').config();
-// let recipe_APP_ID = process.env.Recipe_APP_ID;
-// let recipe_APP_KEY = process.env.Recipe_APP_KEY;
-// let nutrition_APP_ID = process.env.Nutrition_APP_ID;
-// let nutrition_APP_KEY = process.env.Nutrition_APP_KEY;
-// let foodDB_APP_ID = process.env.FoodDB_APP_ID;
-// let foodDB_APP_KEY = process.env.FoodDB_APP_KEY;
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
