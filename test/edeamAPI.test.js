5838008a840496279301ad935e9073e41b63cd8b
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-11-28 23:43:18 -0700

    Add presentation

--- test/edeamAPI.test.js
+++
@@ -1,9 +0,0 @@
-let expect = require("chai").expect;
-let goodstuff = require("../routes/edeamAPI");
-goodstuff.recipeSearch();
-
-describe("recipeSearch", function () {
-    it("should be able to run", function () {
-        expect(true).to.be.true;
-    })
-});
